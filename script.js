6fe16233b9f3bf28da471f91f3254069cfeb0bc0
Safia <69358550+fi-ya@users.noreply.github.com>
2021-03-24 12:42:47 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+//Add tasks to a list so that I can keep track of them
+test("Submitting a new task adds it to the list", () => {
+  // test goes here
+});
+
+//Check things off my list so that I can see what I’ve done
+test("Checking an entry marks it as complete", () => {
+  // test goes here
+});
+
+//Delete things from the list if I don’t need to do them anymore
+
+test("Deleting an entry removes it from the list", () => {
+  // test goes here
+});
+
+//Use all the features of the app without a mouse
+
+//Filter out completed to-dos from my list so that I can focus on what’s left to do
+test("Toggling the filter hides completed tasks from the list", () => {
+  // test goes here
+});
